2ceb35cf906f65d1649ee6ffda43f38e7b08ae02
Cemozerr <cemozer2018@u.northwestern.edu>
2017-07-24 16:02:17 -0400

    Add modified walletsubprovider

---
+++ wallet.js
@@ -0,0 +1,30 @@
+'use strict' 
+const inherits = require('util').inherits
+const HookedWalletEthTxSubprovider = require('web3-provider-engine/subproviders/hooked-wallet-ethtx.js')
+
+module.exports = WalletSubprovider
+
+inherits(WalletSubprovider, HookedWalletEthTxSubprovider)
+
+function WalletSubprovider (wallets, opts) {
+  let addresses;
+  if (Array.isArray(wallets)) {
+    addresses = wallets.map((wallet) => wallet.getAddressString())
+  } else {
+    addresses = [ wallets.getAddressString() ]
+  }
+  opts.getAccounts = function (cb) {
+    cb(null, addresses)
+  }
+
+  opts.getPrivateKey = function (address, cb) {
+    let wallet = wallets[addresses.indexOf(address)]
+    if (wallet) {
+      cb(null, wallet.getPrivateKey())
+    } else {
+      return cb('Account not found')
+    }
+  }
+
+  WalletSubprovider.super_.call(this, opts)
+}

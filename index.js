9d13cb212ff2886f373363aa7fef0925b8442425
Mike Fluff <mf@mikefluff.com>
2018-03-05 01:13:11 +0700

    Ledger subprovider update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var hdkey = require('ethereumjs-wallet/hdkey');
 var Wallet = require('ethereumjs-wallet');
 var ProviderEngine = require("web3-provider-engine");
 var TrezorSubprovider = require("@daonomic/trezor-wallet-provider");
-var LedgerWalletSubproviderFactory = require('ledger-wallet-provider').default;
+var createLedgerSubprovider = require("@ledgerhq/web3-subprovider");
 var FiltersSubprovider = require('web3-provider-engine/subproviders/filters.js');
 var WalletSubprovider = require('web3-provider-engine/subproviders/wallet.js');
 var Web3Subprovider = require("web3-provider-engine/subproviders/web3.js");
@@ -14,7 +14,7 @@ var engine = new ProviderEngine();
 
 let LedgerProvider = function(path, provider_url) {
   
-  var ledgerWalletSubProvider = async LedgerWalletSubproviderFactory(path);
+  var ledgerWalletSubProvider = createLedgerSubprovider(path);
   engine.addProvider(ledgerWalletSubProvider);
   engine.addProvider(new FiltersSubprovider());
   engine.addProvider(new Web3Subprovider(new Web3.providers.HttpProvider(provider_url)));

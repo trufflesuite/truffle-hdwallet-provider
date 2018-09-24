dd4688470435a6de04d8ef30e286a65773d58007
Prophet Daniel <dani_kase@yahoo.com.br>
2018-09-24 16:27:53 -0300

    Update index.js
    Prettified, worked around this bug https://github.com/ethereum/web3.js/issues/1119, var to const whenever possible.

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-var bip39 = require("bip39");
-var hdkey = require('ethereumjs-wallet/hdkey');
-var ProviderEngine = require("web3-provider-engine");
-var FiltersSubprovider = require('web3-provider-engine/subproviders/filters.js');
-var NonceSubProvider = require('web3-provider-engine/subproviders/nonce-tracker.js');
-var HookedSubprovider = require('web3-provider-engine/subproviders/hooked-wallet.js');
-var ProviderSubprovider = require("web3-provider-engine/subproviders/provider.js");
-var Web3 = require("web3");
-var Transaction = require('ethereumjs-tx');
+const bip39 = require("bip39");
+const hdkey = require("ethereumjs-wallet/hdkey");
+const ProviderEngine = require("web3-provider-engine");
+const FiltersSubprovider = require("web3-provider-engine/subproviders/filters.js");
+const NonceSubProvider = require("web3-provider-engine/subproviders/nonce-tracker.js");
+const HookedSubprovider = require("web3-provider-engine/subproviders/hooked-wallet.js");
+const ProviderSubprovider = require("web3-provider-engine/subproviders/provider.js");
+const Web3 = require("web3");
+const Transaction = require("ethereumjs-tx");
 
+Web3.providers.HttpProvider.prototype.sendAsync = Web3.providers.HttpProvider.prototype.send
 // This line shares nonce state across multiple provider instances. Necessary
 // because within truffle the wallet is repeatedly newed if it's declared in the config within a
 // function, resetting nonce from tx to tx. An instance can opt out
 // of this behavior by passing `shareNonce=false` to the constructor.
 // See issue #65 for more
-var singletonNonceSubProvider = new NonceSubProvider();
+let singletonNonceSubProvider = new NonceSubProvider();
 
 function HDWalletProvider(
   mnemonic,
   provider_url,
-  address_index=0,
-  num_addresses=1,
-  shareNonce=true
+  address_index = 0,
+  num_addresses = 1,
+  shareNonce = true
 ) {
   this.mnemonic = mnemonic;
   this.hdwallet = hdkey.fromMasterSeed(bip39.mnemonicToSeed(mnemonic));
@@ -28,9 +29,9 @@ function HDWalletProvider(
   this.wallets = {};
   this.addresses = [];
 
-  for (let i = address_index; i < address_index + num_addresses; i++){
-    var wallet = this.hdwallet.derivePath(this.wallet_hdpath + i).getWallet();
-    var addr = '0x' + wallet.getAddress().toString('hex');
+  for (let i = address_index; i < address_index + num_addresses; i++) {
+    let wallet = this.hdwallet.derivePath(this.wallet_hdpath + i).getWallet();
+    let addr = "0x" + wallet.getAddress().toString("hex");
     this.addresses.push(addr);
     this.wallets[addr] = wallet;
   }
@@ -39,31 +40,43 @@ function HDWalletProvider(
   const tmp_wallets = this.wallets;
 
   this.engine = new ProviderEngine();
-  this.engine.addProvider(new HookedSubprovider({
-    getAccounts: function(cb) { cb(null, tmp_accounts) },
-    getPrivateKey: function(address, cb) {
-      if (!tmp_wallets[address]) { return cb('Account not found'); }
-      else { cb(null, tmp_wallets[address].getPrivateKey().toString('hex')); }
-    },
-    signTransaction: function(txParams, cb) {
-      let pkey;
-      if (tmp_wallets[txParams.from]) { pkey = tmp_wallets[txParams.from].getPrivateKey(); }
-      else { cb('Account not found'); }
-      var tx = new Transaction(txParams);
-      tx.sign(pkey);
-      var rawTx = '0x' + tx.serialize().toString('hex');
-      cb(null, rawTx);
-    }
-  }));
+  this.engine.addProvider(
+    new HookedSubprovider({
+      getAccounts: function(cb) {
+        cb(null, tmp_accounts);
+      },
+      getPrivateKey: function(address, cb) {
+        if (!tmp_wallets[address]) {
+          return cb("Account not found");
+        } else {
+          cb(null, tmp_wallets[address].getPrivateKey().toString("hex"));
+        }
+      },
+      signTransaction: function(txParams, cb) {
+        let pkey;
+        if (tmp_wallets[txParams.from]) {
+          pkey = tmp_wallets[txParams.from].getPrivateKey();
+        } else {
+          cb("Account not found");
+        }
+        let tx = new Transaction(txParams);
+        tx.sign(pkey);
+        let rawTx = "0x" + tx.serialize().toString("hex");
+        cb(null, rawTx);
+      }
+    })
+  );
 
-  (!shareNonce)
+  !shareNonce
     ? this.engine.addProvider(new NonceSubProvider())
     : this.engine.addProvider(singletonNonceSubProvider);
 
   this.engine.addProvider(new FiltersSubprovider());
-  this.engine.addProvider(new ProviderSubprovider(new Web3.providers.HttpProvider(provider_url)));
+  this.engine.addProvider(
+    new ProviderSubprovider(new Web3.providers.HttpProvider(provider_url))
+  );
   this.engine.start(); // Required by the provider engine.
-};
+}
 
 HDWalletProvider.prototype.sendAsync = function() {
   this.engine.sendAsync.apply(this.engine, arguments);
@@ -75,14 +88,17 @@ HDWalletProvider.prototype.send = function() {
 
 // returns the address of the given address_index, first checking the cache
 HDWalletProvider.prototype.getAddress = function(idx) {
-  console.log('getting addresses', this.addresses[0], idx)
-  if (!idx) { return this.addresses[0]; }
-  else { return this.addresses[idx]; }
-}
+  console.log("getting addresses", this.addresses[0], idx);
+  if (!idx) {
+    return this.addresses[0];
+  } else {
+    return this.addresses[idx];
+  }
+};
 
 // returns the addresses cache
 HDWalletProvider.prototype.getAddresses = function() {
   return this.addresses;
-}
+};
 
 module.exports = HDWalletProvider;
